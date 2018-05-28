3bfc09ca5bc1c19b5e699cd2c3be4eea5632523a
Artur Falborski <falborski.artur@gmail.com>
2018-05-28 22:20:48 +0200

    added file/css/img and style webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,17 @@ module.exports = {
         test: /\.tsx?$/,
         loader: "ts-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'url-loader?limit=10000',
+          'img-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
       }
     ]
   }
