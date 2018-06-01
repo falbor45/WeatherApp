29eae2fe25ddeb67603c8369d4432f138e31fed4
Artur Falborski <falborski.artur@gmail.com>
2018-06-01 11:39:16 +0200

    added autocomplete loader

--- src/Autocomplete.tsx
+++ src/Autocomplete.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react'
 import './Autocomplete.css'
 
-export default class Autocomplete extends React.Component<{onClick: any, hidden: boolean, predictions: null | Array<{ name: string, placeId: string}>}, {}> {
+export default class Autocomplete extends React.Component<{onClick: any, loading: boolean, hidden: boolean, predictions: null | Array<{ name: string, placeId: string}>}, {}> {
     render() {
         return (
             <div className="autocomplete-wrapper">
-                {this.props.predictions !== null && !this.props.hidden ? this.props.predictions.map(e => (
+                {
+                    this.props.predictions !== null && !this.props.hidden && !this.props.loading ? this.props.predictions.map(e => (
                     <div className="autocomplete-item"
                          onClick={() => {this.props.onClick(e.name, e.placeId)}}>
                         <p>{e.name}</p>
                     </div>
                 )) : null
+                }
+                {
+                    this.props.loading ? (
+                        <div className="autocomplete-item">
+                            <div className="autocomplete-loader">
+                            </div>
+                        </div>
+                    ) : null
                 }
             </div>
         )
